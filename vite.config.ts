a7ebc96e8712b170a632b9876d188b87a3405638
tgafiulin <t.gafiulin@ya.ru>
2026-02-04 14:31:07 +0300

    Вынос динамических значений конфига в .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['jannette-grimy-shiela.ngrok-free.dev'],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [react()],
+    server: {
+      allowedHosts: env.NGROK_HOST
+        ? [env.NGROK_HOST]
+        : undefined,
+    },
+  }
 })
