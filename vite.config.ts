d98d2b5a323663466c51bce8716491756c536b90
tgafiulin <t.gafiulin@ya.ru>
2026-02-18 17:46:33 +0300

    Тест отключения HMR

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
     base: env.VITE_BASE_URL || '/',
     plugins: [react()],
     server: {
+      hmr: false,
       allowedHosts: env.NGROK_HOST
         ? [env.NGROK_HOST]
         : undefined,
